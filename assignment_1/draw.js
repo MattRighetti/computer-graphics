ac1ae56040f0a79a4e4ff2cd27798ca55544c60a
Mattia Righetti <matt95.righetti@gmail.com>
2020-03-03 01:24:22 +0100

    Assignment 1 Extra
    + Cleaned code

--- assignment_1/draw.js
+++ assignment_1/draw.js
@@ -3,6 +3,11 @@ class Point {
         this.x = x_coordinate;
         this.y = y_coordinate;
     }
+
+    sum(point) {
+        this.x += point.x;
+        this.y += point.y;
+    }
 }
 
 // Points are hardcoded in order
@@ -19,7 +24,7 @@ const orderedPointsArray = [
 
 // Helper function to ease the process
 function join(point_1, point_2) {
-    // line(x1,y1, x2,y2)
+    // line(x1, y1, x2, y2)
 	// draws a line from a point at Normalized screen coordinates x1,y1 to Normalized screen coordinates x2,y2
     line(point_1.x, point_1.y, point_2.x, point_2.y);
 }
@@ -34,44 +39,70 @@ function draw() {
     join(orderedPointsArray[0], orderedPointsArray[orderedPointsArray.length - 1])
 }
 
-// Fills array with angles going from 0° to 360°
-function fillArrayWithAngles(approx_num_of_poligon_sides) {
-    let degree_stepper = 360 / approx_num_of_poligon_sides;
-    var arrayOfAngles = [];
-    var init_degree = 0;
+// Fills array with angles going from initial_angle to final_angle
+function fillArrayWithAnglesFrom(initial_angle, final_angle, polygon_sides_approx) {
+    let total_angle = final_angle - initial_angle;
+    let degree_stepper = total_angle / polygon_sides_approx;
+    var init_val = initial_angle
+    arrayOfAngles = [];
 
-    arrayOfAngles.push(0);
+    arrayOfAngles.push(init_val);
 
-    for(var i = 0; i < approx_num_of_poligon_sides; i++) {
-        init_degree += degree_stepper;
-        arrayOfAngles.push(init_degree);
+    for(var i = 0; i < polygon_sides_approx; i++) {
+        init_val += degree_stepper;
+        arrayOfAngles.push(init_val)
     }
 
     return arrayOfAngles;
 }
 
+// Returns a point given an angle and a radius
 function getPoint(radius, angle) {
     var x = radius * Math.cos(angle * Math.PI / 180);
     var y = radius * Math.sin(angle * Math.PI / 180);
     return new Point(x, y);
 }
 
-function drawCircle(radius, approx_polygon_sides) {
-    arrayOfAngles = fillArrayWithAngles(approx_polygon_sides);
+// Draws a circle given a center_point, radius, from_angle to_angle with an approximation of polygon_sides
+function drawCircle(center_point, radius, from_angle, to_angle, polygon_sides_approx=128) {
+    // Fill array of angles of individual points in sequential order
+    arrayOfAngles = fillArrayWithAnglesFrom(from_angle, to_angle, polygon_sides_approx);
     var point_1, point_2;
 
-    for(var i = 0; i < approx_polygon_sides - 1; i++) {
+    for(var i = 0; i < arrayOfAngles.length - 1; i++) {
+        // Retrieve point given a radius and its angle
         point_1 = getPoint(radius, arrayOfAngles[i]);
+        // Translate point with respect to the center
+        point_1.sum(center_point);
+        
+        // Retrieve next point given a radius and its angle
         point_2 = getPoint(radius, arrayOfAngles[i + 1]);
+        // Translate next point with respect to the center
+        point_2.sum(center_point);
+
+        // Join these points
         join(point_1, point_2);
     }
 
-    point_1 = getPoint(radius, 0);
-
-    join(point_2, point_1);
-
+    // If the angle is 0° to 360° you might want to join the latest and the first point in order
+    // to avoid blank spots due to division imprecision
+    if (from_angle == 0 && to_angle == 360) {
+        point_1 = getPoint(radius, arrayOfAngles[0]);
+        point_1.sum(center_point);
+        join(point_2, point_1);
+    }
 }
 
 function draw2() {
-	drawCircle(0.1, 128);
+    let extra = true;
+
+    if (!extra) {
+        drawCircle(new Point(0.0, 0.0), 0.1, 0, 360);
+    } else {
+        drawCircle(new Point(0.0, -0.4), 0.1, 0, 360);
+        drawCircle(new Point(0.0, 0.4), 0.1, 0, 360);
+        drawCircle(new Point(0.0, 0.4), 0.4, -90, 90);
+        drawCircle(new Point(0.0, -0.4), 0.4, 90, 270);
+        drawCircle(new Point(0.0, 0.0), 0.8, 0, 360);
+    }
 }
