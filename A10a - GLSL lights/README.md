44230a3da3fbd50587f08502ad7a79ccd3b012c1
Mattia Righetti <matt95.righetti@gmail.com>
2020-06-16 02:53:35 +0200

    Assignment 10 almost done

---
+++ A10a - GLSL lights/README.md
@@ -0,0 +1,36 @@
+# Assignment 10a
+
+## Directional 
+<p align="center">
+![directional](img/directional.png)
+</p>
+
+## Point w/o decay 
+<p align="center">
+![point_no_decay](img/point_decay.png)
+</p>
+
+## Directional with ambient
+<p align="center">
+![directional_ambient](img/directional.png)
+</p>
+
+## Point w/ decay 
+<p align="center">
+![point_decay](img/directional.png)
+</p>
+
+## Spotlight with decay 
+<p align="center">
+![spotlight_decay](img/directional.png)
+</p>
+
+## Directional with hemispheric ambient
+<p align="center">
+![directional_hemispheric](img/directional.png)
+</p>
+
+## Three lights: a directional, a point and a spot
+<p align="center">
+![directional](img/directional.png)
+</p>
--- A10a - GLSL lights/shaders.js
+++ A10a - GLSL lights/shaders.js
@@ -1,49 +1,49 @@
 function shaders() {
 // The shader can find the required informations in the following variables:
 
-//vec3 fs_pos;			// Position of the point in 3D space
-//
-//vec3 LAPos;			// Position of first (or single) light
-//vec3 LADir;			// Direction of first (or single) light
-//float LAConeOut;		// Outer cone (in degree) of the light (if spot)
-//float LAConeIn;		// Inner cone (in percentage of the outher cone) of the light (if spot)
-//float LADecay;		// Decay factor (0, 1 or 2)
-//float LATarget;		// Target distance
-//vec4 LAlightColor;	// color of the first light
-//		
-//vec3 LBPos;			// Same as above, but for the second light
-//vec3 LBDir;
-//float LBConeOut;
-//float LBConeIn;
-//float LBDecay;
-//float LBTarget;
-//vec4 LBlightColor;
-//
-//vec3 LCPos;			// Same as above, but for the third one
-//vec3 LCDir;
-//float LCConeOut;
-//float LCConeIn;
-//float LCDecay;
-//float LCTarget;
-//vec4 LClightColor;
-//
-//vec4 ambientLightColor;		// Ambient light color. For hemispheric, this is the color on the top
-//vec4 ambientLightLowColor;	// For hemispheric ambient, this is the bottom color
-//vec3 ADir;					// For hemispheric ambient, this is the up direction
-//
-//vec3 normalVec;				// direction of the normal vector to the surface
-//
-//
+// vec3 fs_pos;			// Position of the point in 3D space
+// 
+// vec3 LAPos;			// Position of first (or single) light
+// vec3 LADir;			// Direction of first (or single) light
+// float LAConeOut;		// Outer cone (in degree) of the light (if spot)
+// float LAConeIn;		// Inner cone (in percentage of the outher cone) of the light (if spot)
+// float LADecay;		// Decay factor (0, 1 or 2)
+// float LATarget;		// Target distance
+// vec4 LAlightColor;	// color of the first light
+// 		
+// vec3 LBPos;			// Same as above, but for the second light
+// vec3 LBDir;
+// float LBConeOut;
+// float LBConeIn;
+// float LBDecay;
+// float LBTarget;
+// vec4 LBlightColor;
+// 
+// vec3 LCPos;			// Same as above, but for the third one
+// vec3 LCDir;
+// float LCConeOut;
+// float LCConeIn;
+// float LCDecay;
+// float LCTarget;
+// vec4 LClightColor;
+// 
+// vec4 ambientLightColor;		// Ambient light color. For hemispheric, this is the color on the top
+// vec4 ambientLightLowColor;	// For hemispheric ambient, this is the bottom color
+// vec3 ADir;					// For hemispheric ambient, this is the up direction
+// 
+// vec3 normalVec;				// direction of the normal vector to the surface
+// 
+// 
 // Final direction and colors are returned into:
-//vec3 lightDirA;
-//vec3 lightDirB;
-//vec3 lightDirC;
-//
-//and intensity is returned into:
-//
-//vec4 lightColorA;
-//vec4 lightColorB;
-//vec4 lightColorC;
+// vec3 lightDirA;
+// vec3 lightDirB;
+// vec3 lightDirC;
+// 
+// and intensity is returned into:
+// 
+// vec4 lightColorA;
+// vec4 lightColorB;
+// vec4 lightColorC;
 //
 // Ambient light contribution is returned into
 //
@@ -51,8 +51,8 @@ function shaders() {
 
 // Single directional light
 var S1 = `
-	lightDirA = LADir;
-	lightColorA = LAlightColor;
+    lightDirA = LADir;
+    lightColorA = LAlightColor;
 `;
 
 // Single point light without decay
@@ -63,33 +63,73 @@ var S2 = `
 
 // Single directional light, constant ambient
 var S3 = `
-	lightDirA   = vec3(1.0, 0.0, 0.0);
-	lightColorA = vec4(1.0, 0.0, 0.0, 1.0);
+    lightDirA = LADir;
+    lightColorA = LAlightColor;
+    ambientColor = ambientLightColor;
 `;
 
 // Single point light with decay
 var S4 = `
-	lightDirA   = vec3(1.0, 0.0, 0.0);
-	lightColorA = vec4(0.0, 0.0, 1.0, 1.0);
+    // as in single point
+    lightDirA = normalize(LAPos - fs_pos);
+
+    // 1 / |p - x|
+    float vecModule = distance(LAPos, fs_pos);
+    // (g / |p - x|)^Beta
+    float powerVec = pow(LATarget/vecModule, LADecay);
+    // l * (g / |p - x|)^Beta
+	lightColorA = LAlightColor * powerVec;
 `;
 
 // Single spot light (with decay)
 var S5 = `
-	lightDirA   = vec3(1.0, 0.0, 0.0);
-	lightColorA = vec4(1.0, 0.0, 1.0, 1.0);
+    lightDirA = normalize(LAPos - fs_pos);
+    
+    // DECAY
+
+    float vecModule = distance(LAPos, fs_pos);
+    float decay = pow(LATarget/vecModule, LADecay);
+
+    // CONE
+
+    float LAConeOutRad = radians(LAConeOut / 2.0);
+    float LAConeInRad = radians((LAConeIn * LAConeOut) / 2.0);
+    float outerCone = cos(LAConeOutRad);
+    float innerCone = cos(LAConeInRad);
+    float coneLightZone = (( dot(lightDirA, LADir) - outerCone) / (innerCone - outerCone));
+    float clampedConeLightZone = clamp(coneLightZone, 0.0, 1.0);
+
+    lightColorA = LAlightColor * decay * clampedConeLightZone;
 `;
 
 // Single directional light, hemispheric ambient 
 var S6 = `
-	lightDirA   = vec3(1.0, 0.0, 0.0);
-	lightColorA = vec4(0.0, 1.0, 1.0, 1.0);
+    lightDirA = LADir;
+    
+    float nx_dot_d = dot(normalVec, LADir);
+    vec4 lu_vec = ((nx_dot_d + 1.0) / 2.0) * ambientLightColor;
+    vec4 ld_vec = ((1.0 - nx_dot_d) / 2.0) * ambientLightLowColor;
+    vec4 la_vec = lu_vec + ld_vec;
+
+    lightColorA = LAlightColor;
+    ambientColor = vec4(cross(la_vec.xyz, ADir), la_vec.w);
 `;
 
 // Three lights: a directional, a point and a spot
 var S7 = `
-	lightDirA   = vec3(1.0, 0.0, 0.0);
-	lightColorA = vec4(1.0, 1.0, 1.0, 1.0);
+    // LIGHT A
+    lightDirA = LADir;
+    lightColorA = LAlightColor;
+
+    // LIGHT B
+    lightDirB = normalize(LBPos - fs_pos);
+    lightColorB = LBlightColor;
+    
+    // LIGHT C
+    lightDirC = normalize(LCPos - fs_pos);
+    lightColorC = 
 `;
-	return [S1, S2, S3, S4, S5, S6, S7];
+
+return [S1, S2, S3, S4, S5, S6, S7];
 }
 
